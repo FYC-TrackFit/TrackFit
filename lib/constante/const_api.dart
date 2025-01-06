69ad9655f0a7abafe33505426a26f8dd2d6fe557
Yannick Boyer <yannickboyer25@gmail.com>
2025-01-06 10:57:13 +0100

    Correct ip for emulator

--- lib/constante/const_api.dart
+++ lib/constante/const_api.dart
@@ -1,4 +1,4 @@
 class ConstApi {
   // static const url = "http://192.168.143.160:44318/api";
-  static const url = "http://10.0.0.2:44318/api";
+  static const url = "http://10.0.2.2:44318/api";
 }
